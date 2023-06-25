c75dabf5922237958c04d05a328fd5d613ad60b3
Keagan Dickinson <98011652+PhisherFTW@users.noreply.github.com>
2023-06-25 17:22:47 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const btns = document.querySelectorAll(".acc-btn");
+
+// fn
+function accordion() {
+  // this = the btn | icon & bg changed
+  this.classList.toggle("is-open");
+
+  // the acc-content
+  const content = this.nextElementSibling;
+  
+  // IF open, close | else open
+  if (content.style.maxHeight) content.style.maxHeight = null;
+  else content.style.maxHeight = content.scrollHeight  + "px";
+  
+}
+// event
+btns.forEach((el) => el.addEventListener("click", accordion));
+
+
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
